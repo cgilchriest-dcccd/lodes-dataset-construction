c42a41d8390ced290913fbc59868ba3b8bcf2a11
camille gilchriest <cgilchriest.dcccd@gmail.com>
2024-11-22 10:56:58 -0600

    add function to update existing db

--- examples/update_existing.py
+++ examples/update_existing.py
@@ -11,7 +11,7 @@ from build_database import *
 import os
 
 #define paths
-wkd = r"C:\Users\cmg0003\Desktop\TX_Lodes_Download"
+wkd = r"C:\Users\cmg0003\Desktop"
 spath = os.path.join(wkd,"lodes_tx_slim.db")
 
 # --- processing
@@ -20,18 +20,16 @@ fps = get_all_possible_files(save=True,
                              savepath=wkd,
                              savename="state_dict")
 
+#just download 2022
+fouts = filter_for_single_year_files(links_dict=fps,year='2022',state_cd='tx')
 
-#this downloads everything in that state's lodes
-state_fold = download_state_lodes_file(save_loc=wkd,
-                          st='tx',
-                          links_dict=fps)
 
 #this unzips everything 
 unzip_state_lodes_file(state_fold= state_fold)
 
 
 # loads downloaded data into spatialite 
-build_db(spath=spath) #be careful - this build function overwrites existing data
+#build_db(spath=spath) #be careful - this build function overwrites existing data
 #state_fold = r"C:\Users\cmg0003\Desktop\TX_Lodes_Download\tx"
 load_lodes_into_db(folder_path = state_fold,spath = spath, base_only = True)
-load_geometries_into_db(spath=spath) #note - this is basically a custom function for texas geometries - will need work for other states
+
